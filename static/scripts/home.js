a3863d857bd4f61911c0d22ef54d1f83cb02ce21
EucTech <euccreative@gmail.com>
2024-02-09 03:43:46 +0100

    updated  home.js

--- static/scripts/home.js
+++ static/scripts/home.js
@@ -13,12 +13,46 @@ $(".testimonies .quotes").owlCarousel({
         nav: true,
         loop: true,
       },
-      580: {
+      700: {
+        items: 2,
+        nav: true,
+        loop: true,
+      },
+      800: {
+          items: 2,
+          nav: true,
+          loop: true,
+        },
+      1200: {
+        items: 3,
+        nav: true,
+        loop: true,
+      },
+    },
+  });
+
+
+  $(".works .main").owlCarousel({
+    items: 3,
+    loop: true,
+    autoplay: true,
+    margin: 50,
+    // dots: true,
+    arrows: true,
+    nav: false,
+    navText: ["<i class='fa fa-chevron-left'></i>", "<i class='fa fa-chevron-right'></i>"],
+    responsive: {
+      0: {
         items: 1,
         nav: true,
         loop: true,
       },
-      768: {
+      700: {
+        items: 2,
+        nav: true,
+        loop: true,
+      },
+      800: {
           items: 2,
           nav: true,
           loop: true,
