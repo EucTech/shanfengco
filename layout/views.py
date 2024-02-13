30a73b08cb6f52de64a6187ccfb911b9384fcec3
EucTech <euccreative@gmail.com>
2024-02-13 06:19:03 +0100

    view

--- layout/views.py
+++ layout/views.py
@@ -1,10 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import MessageForm, NewsletterForm
+from django.contrib import messages
+from django import forms
+from django.core.exceptions import ValidationError
 # from django.http import HttpResponse
 
-def home(request):
-    context = {'title': 'Shan feng tire repair company'}
-    return render(request, 'home.html', context)
 
+def home(request):
+    form = MessageForm()
+    news_form = NewsletterForm()
+
+    if request.method == 'POST':
+        if 'message-submit' in request.POST:
+            form = MessageForm(request.POST)
+            if form.is_valid():
+                form.save()
+                messages.success(request, 'Your message has been sent successfully!')
+                return redirect('home')
+        
+        elif 'newsletter-submit' in request.POST:
+            news_form = NewsletterForm(request.POST)
+            
+            if news_form.is_valid():   
+                news_form.save()
+                messages.success(request, 'You have successfully subscribed to the newsletter!')
+                return redirect('home')
+
+    context = {'title': 'Shan feng tire repair company'}
+    return render(request, 'home.html', {'form': form, 'news_form': news_form, 'context': context})
 
 def services(request):
     context = {'title': 'Services | Shan feng tire repair company'}
@@ -17,5 +40,12 @@ def about(request):
 
 
 def contact(request):
+    form = MessageForm()
+    if request.method == 'POST':
+        form = MessageForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your message has been sent successfully!')
+            return redirect('contact')
     context = {'title': 'contact | Shan feng tire repair company'}
-    return render(request, 'contact.html', context)
+    return render(request, 'contact.html', {'form': form, 'context': context})
