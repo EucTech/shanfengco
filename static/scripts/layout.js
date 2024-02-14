1ffd3a70c35d1bc1cf20032f11b2774d7ab6e938
EucTech <euccreative@gmail.com>
2024-02-14 22:34:27 +0100

    updated

--- static/scripts/layout.js
+++ static/scripts/layout.js
@@ -4,58 +4,138 @@ const navlinksA = document.querySelectorAll('.navlinks a');
 const navlinks = document.querySelector('.navlinks');
 
 function OpenNav() {
-    navlinks.style.top ="5em";
-    navOpen.style.display = "none";
-    navClose.style.display = "block";
+  navlinks.style.top = "5em";
+  navOpen.style.display = "none";
+  navClose.style.display = "block";
 }
 
 function CloseNav() {
-    navlinks.style.top ="-100em";
-    navClose.style.display = "none";
-    navOpen.style.display = "block";
+  navlinks.style.top = "-100em";
+  navClose.style.display = "none";
+  navOpen.style.display = "block";
 }
 
-navOpen.addEventListener('click', function() {
-    OpenNav();
+navOpen.addEventListener('click', function () {
+  OpenNav();
 });
 
-navClose.addEventListener('click', function() {
-    CloseNav();
+navClose.addEventListener('click', function () {
+  CloseNav();
 });
 
 navlinksA.forEach(links => {
-    links.addEventListener('click', function() {
-        CloseNav();
-    })
+  links.addEventListener('click', function () {
+    CloseNav();
+  })
 })
 
 
-  // Get the button:
-  let mybutton = document.getElementById("myBtn");
+// Get the button:
+let mybutton = document.getElementById("myBtn");
 
-  // When the user scrolls down 20px from the top of the document, show the button
-  window.onscroll = function() {scrollFunction()};
-  
-  function scrollFunction() {
-    if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
-      mybutton.style.display = "block";
-    } else {
-      mybutton.style.display = "none";
-    }
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction()
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
   }
-  
-  // When the user clicks on the button, scroll to the top of the document
-  function topFunction() {
-    // Scroll to the top of the document smoothly
-    document.body.scrollTo({ top: 0, behavior: 'smooth' }); // For Safari
-    document.documentElement.scrollTo({ top: 0, behavior: 'smooth' }); // For Chrome, Firefox, IE and Opera
-  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  // Scroll to the top of the document smoothly
+  document.body.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  }); // For Safari
+  document.documentElement.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  }); // For Chrome, Firefox, IE and Opera
+}
+
+// To hide the messages after 5 seconds
+// setTimeout(function () {
+//   document.querySelectorAll('.messages').forEach(function (el) {
+//     el.style.display = 'none';
+//   });
+// }, 5000);
 
 
 
-  // To hide the messages after 5 seconds
-  setTimeout(function() {
-    document.querySelectorAll('.messages').forEach(function(el) {
-      el.style.display = 'none';
+// For message section
+
+$(document).ready(function () {
+  $('#myForm').submit(function (event) {
+    event.preventDefault();
+
+    // get submit original value in html
+    let submitvalue = $('#message-submit').val();
+
+    // set the value to loading when submiting
+    $('#message-submit').val('Loading.......');
+
+    let formData = $(this).serialize();
+    console.log(formData);
+
+    $.ajax({
+      type: 'POST',
+      url: '/home/',
+      data: formData,
+      success: function (response) {
+        console.log('Message sent successfully');
+        $('#message').text('Your message has been sent successfully!').show();
+        // Clear form values
+        $('#myForm')[0].reset();
+      },
+      error: function (xhr, status, error) {
+        console.error('Error:', error)
+      },
+      complete: function () {
+        $('#message-submit').val(submitvalue);
+      }
     });
-  }, 5000); 
+  });
+});
+
+
+
+// For newsletter section
+
+$(document).ready(function () {
+  $('#mynewsletter').submit(function (event) {
+    event.preventDefault();
+
+    // get submit original value in html
+    let submitvalue = $('#newsletter-submit').val();
+
+    // set the value to loading when submiting
+    $('#newsletter-submit').val('Loading.......');
+
+    let formData = $(this).serialize();
+    console.log(formData);
+
+    $.ajax({
+      type: 'POST',
+      url: '/home/',
+      data: formData,
+      success: function (response) {
+        console.log('Message sent successfully');
+        $('#news-message').text('You have subscribed to our newsletter!').show();
+        // Clear form values
+        $('#mynewsletter')[0].reset();
+      },
+      error: function (xhr, status, error) {
+        console.error('Error:', error)
+      },
+      complete: function () {
+        $('#newsletter-submit').val(submitvalue);
+      }
+    });
+  });
+});
