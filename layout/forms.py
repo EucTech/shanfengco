1a02df358f05c5495e9e4ceeed04cc307f187142
EucTech <euccreative@gmail.com>
2024-02-20 01:45:10 +0100

    updated

--- layout/forms.py
+++ layout/forms.py
@@ -20,19 +20,11 @@ class MessageForm(forms.ModelForm):
 class NewsletterForm(forms.ModelForm):
     class Meta:
         model = Newsletter
-        fields = ['full_name', 'email', 'phone', 'company']
+        fields = ['full_name', 'email', 'company']
 
         widgets = {
             'full_name': forms.TextInput(attrs={'placeholder': 'Full Name'}),
             'email': forms.EmailInput(attrs={'placeholder': 'Email'}),
-            'phone': forms.TextInput(attrs={'placeholder': 'Phone'}),
             'company': forms.TextInput(attrs={'placeholder': 'Company'}),
         }
-
-    # def clean_email(self):
-    #     """This is to check if there is two emails"""
-    #     email = self.cleaned_data['email']
-    #     if Newsletter.objects.filter(email=email).exists():
-    #         raise forms.ValidationError("Email address already exists.")
-    #     return email
 
